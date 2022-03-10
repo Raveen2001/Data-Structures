95c4fb0bd7121f6e271ffa1e87331ed9278fbe9f
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-10 10:48:10 +0530

    binary tree insert completed

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class BinaryTree {
+public class BinaryTree {
+    private class Node{
+        int value;
+        Node leftChild;
+        Node rightChild;
+
+        Node(int value){
+            this.value = value;
+        }
+
+        @Override
+        public String toString(){
+            return ("Node=" + this.value);
+        }
+    }
+
+    private Node root;
+
+
+    public void insert(int value){
+
+        insert(root, value);
+
+    }
+
+    public  void insert(Node node, int value){
+        if(node == null){
+            root = new Node(value);
+            return;
+        }
+
+        if(node.value > value){
+            if(node.leftChild == null){
+                node.leftChild = new Node(value);
+                return;
+            }
+            insert(node.leftChild, value);
+        }else{
+            if(node.rightChild == null){
+                node.rightChild = new Node(value);
+                return;
+            }
+            insert(node.rightChild, value);
+        }
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        BinaryTree tree = new BinaryTree();
+        tree.insert(10);
+        tree.insert(9);
+        tree.insert(8);
+        tree.insert(5);
+        tree.insert(12);
+        tree.insert(11);
+        tree.insert(15);
+        tree.insert(89);
+        System.out.println("Done");
+
+    }
 }
